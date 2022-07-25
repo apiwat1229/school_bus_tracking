ff8a8c30765f3ac751d2a923a3256cd29e1414dc
apiwat1229 <inwaui1229@gmail.com>
2022-07-25 20:51:40 +0700

    Edit Title App Text
    edit Title App Text

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Wellcome Google Map',
+              'Wellcome SchoolBus Tracking App',
             ),
           ],
         ),
